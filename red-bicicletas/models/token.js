1d355a3a5baa5e8fa1f8723148fc62d18a052b69
Michell Bravo <eduardoqb24@hotmail.com>
2021-03-11 21:50:39 -0500

    Metodos de validacion y token

---
+++ red-bicicletas/models/token.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var tokenSchema = new Schema({
+    _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Usuario' },
+    token: { type: String, required: true },
+    createdA: { type: Date, required: true, default: Date.now, expires: 43200 }
+});
+
+module.exports = mongoose.model('Token, tokenSchema');
--- red-bicicletas/models/usuario.js
+++ red-bicicletas/models/usuario.js
@@ -1,10 +1,55 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 var Reserva = require('../models/reserva');
+var bcrypt = require('bcryp');
+
+const saltRound = 10;
 
 var usuarioSchema = new Schema({
-    nombre: String
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        unique: true,
+        validate: [validateEmail, 'Debe ingresar un email valido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.plugin(uniqueValidator, { message:'El {PATH} ya existe con otro usuario' });
+
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+}
+
+usuarioSchema.pre('save', function(next){
+    if( this.isModified('password') ){
+        this.password = bcrypt.hashSync( this.password, saltRounds );
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}
 
 usuarioSchema.statics.allUsuarios = function(){
     return this.find({});
